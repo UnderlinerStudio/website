0bf5c4e89f8641a578265e548d5b00dac0ac8c24
Jan Vítů <vitu.jan1@gmail.com>
2024-02-01 12:45:28 +0100

    Homepage animations update

--- hugo/assets/ts/homepage.ts
+++ hugo/assets/ts/homepage.ts
@@ -1,4 +1,4 @@
-import { stagger } from "motion";
+import { animate, stagger } from "motion";
 import { $, $$ } from "./selector";
 import {
 	homeGridReveal,
@@ -6,7 +6,7 @@ import {
 	scrollDownAnimate,
 } from "./animations";
 
-const heroSection = document.getElementById("hero");
+const heroSection = document.getElementById("hero-content");
 const scroll = $("[data-animate='after']");
 
 const heading2 = $("[data-animate='h2-from-bottom'");
@@ -14,12 +14,25 @@ const heading2Elements = heading2.querySelectorAll(
 	"[data-animate='from-bottom']",
 ) as NodeListOf<HTMLElement>;
 
+const heroAnimations = heroSection.querySelectorAll("[data-animate]");
+
+if (heroAnimations)
+	heroAnimations.forEach((entry) => {
+		animate(
+			entry,
+			{
+				opacity: [0, 1],
+			},
+			{ duration: 0.6, delay: 0.4, easing: "ease-in-out" },
+		);
+	});
+
 const grid = $(".hero-grid");
 const elements = grid.childNodes;
 homeGridReveal(elements);
 scrollDownAnimate(scroll, heroSection);
 
-const heading1ObserverOptions: IntersectionObserverInit = {
+const heading2ObserverOptions: IntersectionObserverInit = {
 	rootMargin: "",
 	threshold: 0.7,
 };
@@ -28,20 +41,15 @@ observerAnimation({
 	boundingElement: heading2,
 	targetElement: heading2Elements,
 }).fromDown({
-	observerOptions: heading1ObserverOptions,
+	observerOptions: heading2ObserverOptions,
 	duration: 0.5,
 	delay: stagger(0.15),
 	easing: "ease-in-out",
 });
 
-const fadeInElements = $("[data-animate='fade-in']");
+const fadeInElements = $("[data-bounding]");
 
 observerAnimation({
 	boundingElement: fadeInElements,
 	targetElement: fadeInElements,
 }).fadeIn({ duration: 0.3, delay: 0.2, easing: "ease-in-out" });
-
-const observerOptions: IntersectionObserverInit = {
-	rootMargin: "",
-	threshold: 0.6,
-};
